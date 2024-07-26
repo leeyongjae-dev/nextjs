bdd66c340808152ff9ddc9c8033c8e5d8472db95
BOOK-BKT8UBVE0A\dhfkd <dhfkdl26@naver.com>
2024-07-26 09:47:51 +0900

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin'] });
+export const inter2 = Inter({ subsets: ['latin-ext'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });

2deb590a0f98445446557ef9529ee982328dbedc
LEE <dhfkdl26@naver.com>
2024-08-07 17:14:49 +0900

    target 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,80 +1,55 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import styles from '@/app/ui/home.module.css'
-import { lusitana } from '@/app/ui/fonts'
-import Image from 'next/image'
+import AcmeLogo from "@/app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function Page() {
-
   const urlLink = [
     {
-        url : '/dashboard'
-      , name : 'Dashboard'
-    } ,
-    {
-        url : 'https://mail.worksmobile.com'
-      , name : 'Mail'
+      url: "/dashboard",
+      name: "Dashboard",
+      target: "N",
     },
     {
-        url : 'http://gw.interplug.co.kr/main/platformFrame.do'
-      , name : 'Groupware'
+      url: "https://mail.worksmobile.com",
+      name: "Mail",
+      target: "Y",
     },
     {
-        url : 'http://1.248.227.176:43333/redmine/'
-      , name : 'Redmine'
+      url: "http://gw.interplug.co.kr/main/platformFrame.do",
+      name: "Groupware",
+      target: "Y",
+    },
+    {
+      url: "http://1.248.227.176:43333/redmine/",
+      name: "Redmine",
+      target: "Y",
     },
     /* 필요시 추가 */
-  ]
+  ];
 
   return (
     <main className="flex min-h-screen flex-col p-6">
       {/* <div className={styles.shape}/> */}
-      
+
       <div className="flex h-20 shrink-0 rounded-lg bg-gray-200 p-4 md:h-52">
         <AcmeLogo />
       </div>
 
       <div className="mt-4 flex grow flex-col gap-4">
         <div className="flex flex-col justify-center rounded-lg bg-gray-50 px-6 py-10">
-        {/* <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/> */}
-          {/* <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to the show.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p> */}
-          
           {/* DashBoard In */}
           {urlLink.map((data) => (
             <Link
               key={data.name}
               href={data.url}
               className="flex items-center gap-5 self-start rounded-lg px-6 py-3 text-sm font-medium transition-colors hover:bg-gray-200 md:text-base"
-              style={{width:"100%"}}
+              style={{ width: "100%" }}
+              target={data.target === "Y" ? "_blank" : ""}
             >
               <span>{data.name}</span> <ArrowRightIcon className="w-5 md:w-6" />
-            </Link> 
+            </Link>
           ))}
-
         </div>
-        {/* <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className="hidden md:hidden"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
-        </div> */}
       </div>
     </main>
   );

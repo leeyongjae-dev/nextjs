295fe1e8e00c635f212eba6d63bf2f6da34ecb6e
LEE <dhfkdl26@naver.com>
2024-08-13 16:01:06 +0900

    빈파일로 수정

--- app/dashboard/board/page.tsx
+++ app/dashboard/board/page.tsx
@@ -1,20 +1,26 @@
-import { FormattedBoardTable } from "@/app/lib/definitions";
 import Table from "@/app/ui/board/table";
 
-export default async function Page({
-  searchParams,
-  boardList,
-}: {
-  searchParams?: {
-    query?: string;
-    page?: string;
-    per?: string;
-  };
-  boardList: FormattedBoardTable[];
-}) {
+export default async function Page() {
   return (
     <div className="w-full">
-      <Table boardList={boardList} totalPages={0} />
+      <Table boardList={[]} totalPages={0} />
     </div>
   );
 }
+// {
+//   searchParams,
+//   boardList,
+// }: {
+//   searchParams?: {
+//     query?: string;
+//     page?: string;
+//     per?: string;
+//   };
+//   boardList: FormattedBoardTable[];
+// }) {
+//   return (
+//     <div className="w-full">
+//       <Table boardList={boardList} totalPages={0} />
+//     </div>
+//   );
+// }
